5264e035572d741ced7ff99638bc9672f6ad60bf
NNN3103m <nnn3103m@gmail.com>
2020-10-30 00:51:25 -0600

    Creacion de pantallas
    Cree las pantallas de seleccion de habilidad y las de las habilidades, en la de seleccion de habilidad puse los botones y los nombre de acuerdo al string.xml

---
+++ app/src/main/java/project/superheroapp/combat.kt
@@ -0,0 +1,11 @@
+package project.superheroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class combat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_combat)
+    }
+}
---
+++ app/src/main/java/project/superheroapp/stats_select.kt
@@ -0,0 +1,11 @@
+package project.superheroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class stats_select : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stats_select)
+    }
+}
