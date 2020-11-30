156109bf9c3fce47c0014e51395009e3d20b88c1
brix <brix@nice.com>
2020-11-30 17:07:00 -0600

    commit para ver si se puede salvar el semestre

--- app/src/main/java/project/superheroapp/SupHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.app.superheroapp2
-
-import android.widget.ImageView
-import android.media.Image
-import com.squareup.picasso.Picasso
-
-fun ImageView.fromUrl(url:String){
-    Picasso.get().load(url).into(this)
-}
